c0aa93f0e0ba896eb7e5b8f8a49d93a9c75c2c61
kamoun said <said.kamoun@gmail.com>
2022-11-10 00:12:31 +0100

    add observable to app component to read server service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, pipe, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { AppState } from './interface/app.stat';
+import { CustomResponse } from './interface/custom.response';
+import { ServerService } from './service/server.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'serverFront';
+export class AppComponent implements OnInit {
+ 
+  appState$ :Observable<AppState<CustomResponse>> ;
+  constructor(private serverService:ServerService){}
+  ngOnInit(): void {
+    this.appState$= this.serverService.servers$
+       .pipe(
+        map(resp=>{
+       return {dataState:DataState.LOADED_STATE,
+        appData:resp}
+        }),
+        startWith({dataState:DataState.LOADING_STATE}),
+        catchError((error:string)=>{
+          return of({dataState:DataState.ERROR_STATE,error
+        })
+      })
+        ); 
+  }
 }
