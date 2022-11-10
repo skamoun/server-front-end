916eb67e73bc7a0395b4691fed035635ea73ff95
kamoun said <said.kamoun@gmail.com>
2022-11-11 00:32:52 +0100

    fix bug in server service observables

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -18,16 +18,16 @@ export class ServerService {
      catchError(this.handleError) 
     );
 
-    server$=(server:Server)=><Observable<CustomResponse>>this.http.post<CustomResponse>('${this.apiUrl}/server/save',server).pipe(
+    server$=(server:Server)=><Observable<CustomResponse>>this.http.post<CustomResponse>(`${this.apiUrl}/server/save`,server).pipe(
       tap(console.log),
       catchError(this.handleError) 
      );
-     ping$=(ipAddress:string)=><Observable<CustomResponse>>this.http.get<CustomResponse>('${this.apiUrl}/server/ping/${ipAddress}').pipe(
+     ping$=(ipAddress:string)=><Observable<CustomResponse>>this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAddress}`).pipe(
       tap(console.log),
       catchError(this.handleError) 
      );
 
-     delete$=(serverId:number)=><Observable<CustomResponse>>this.http.delete<CustomResponse>('${this.apiUrl}/server/delete/${serverId}').pipe(
+     delete$=(serverId:number)=><Observable<CustomResponse>>this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`).pipe(
       tap(console.log),
       catchError(this.handleError) 
      );
@@ -35,9 +35,9 @@ export class ServerService {
       subscriber=>{
         console.log(response);
         subscriber.next(
-          status===Status.ALL?{...response,message:'servers filterd by ${status} status '}:
+          status===Status.ALL?{...response,message:`servers filterd by ${status} status `}:
           {...response,
-            message: response.data.servers.filter(server=>server.status===status).length>0?'servers filterd by ${status==Status.SERVER_DOWN? server_down:serevr_up} status ':' no servers with ${status} status',
+            message: response.data.servers.filter(server=>server.status===status).length>0?`servers filterd by ${status==Status.SERVER_DOWN? 'server_down':'serevr_up'} status `:` no servers with ${status} status`,
             data:{servers:response.data.servers?.filter(server=>server.status===status)}
 
           }
@@ -50,6 +50,6 @@ export class ServerService {
      );
    private handleError(error: HttpErrorResponse): Observable<never> {
     console.log(error);
-      return throwError('an error occured-error code: ${error.status}');
+      return throwError(`an error occured-error code: ${error.status}`);
     }
 }
